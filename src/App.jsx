6794c09239e05c282500db9ad2b2afcdf12bbfae
osama2kabdullah <abdullah21673@hotmail.com>
2023-04-04 15:16:23 +0600

    remember old state

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,86 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
-const btnCSS = 'bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-1 border border-blue-500 hover:border-transparent rounded'
+const btnCSS =
+  "bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-1 border border-blue-500 hover:border-transparent rounded";
 
 function App() {
+  //[states]
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(false);
   const [winner, setWinner] = useState(null);
+  const [previusStates, setPreviusStates] = useState([]);
+  const [noOneWin, setNoOneWin] = useState(false);
 
+  //reset game
   function resetGame() {
     setSquares(Array(9).fill(null));
     setWinner(null);
+    setPreviusStates([]);
   }
 
+  // detect no one win and all squares filled
+  useEffect(()=>{
+    if(!winner && !squares.includes(null)){
+      setNoOneWin(true);
+    }
+  },[!winner && !squares.includes(null)])
+
   function squareClick(index) {
-    if(winner){
+    // check if anyone win or no one win
+    if (winner || noOneWin) {
       return;
     }
+    //make a compy of original state and push it remember state
     const newSquares = [...squares];
-    if(xIsNext){
-      newSquares[index] = 'X';
-    }else {
-      newSquares[index] = 'Y';
+    setPreviusStates([...previusStates, newSquares]);
+    //switching players
+    if (xIsNext) {
+      newSquares[index] = "X";
+    } else {
+      newSquares[index] = "Y";
     }
+    //set updated squres for render DOM
     setSquares(newSquares);
     setXIsNext(!xIsNext);
-    setWinner(findWonPerson(newSquares));
+    //check is anyone win
+    if(findWonPerson(newSquares)){
+      setWinner(findWonPerson(newSquares));
+    }
+  }
+
+  //get back previus game state
+  function handlePreState(prState) {
+    setSquares(prState);
   }
 
   return (
     <div className="w-[25%] mx-auto">
       <p>{winner && `${winner} is winner`}</p>
-      {winner && <button className={btnCSS} onClick={resetGame}>reset game</button>}
+      {winner && (
+        <button className={btnCSS} onClick={resetGame}>
+          reset game
+        </button>
+      )}
       <div className="grid grid-cols-3 gap-[1px] border-2 text-center bg-black">
-        {
-          squares.map((square, index)=><div onClick={()=>squareClick(index)} className="bg-white cursor-pointer">{square || <span>&nbsp;</span>}</div>)
-        }
+        {squares.map((square, index) => (
+          <div
+            onClick={() => squareClick(index)}
+            className="bg-white cursor-pointer"
+          >
+            {square || <span>&nbsp;</span>}
+          </div>
+        ))}
+      </div>
+      
+      <div>
+        {previusStates.map((prState, i) => (
+          <li>
+            <button onClick={() => handlePreState(prState)} className={btnCSS}>
+              previus state {i+1}
+            </button>
+          </li>
+        ))}
       </div>
     </div>
   );
@@ -44,20 +89,21 @@ function App() {
 export default App;
 
 const possibleIndexes = [
-  [0,1,2],
-  [0,3,6],
-  [0,4,8],
-  [1,4,7],
-  [2,4,6],
-  [2,5,8],
-  [3,4,5],
-  [6,7,8],
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 4, 6],
+  [2, 5, 8],
+  [3, 4, 5],
+  [6, 7, 8],
 ];
 
+//check anyone won or not
 function findWonPerson(squares) {
   for (const iterator of possibleIndexes) {
     const [a, b, c] = iterator;
-    if(squares[a] === squares[b] && squares[b] === squares[c]){
+    if (squares[a] === squares[b] && squares[b] === squares[c]) {
       return squares[a];
     }
   }
